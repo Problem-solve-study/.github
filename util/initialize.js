c5a8faacf1f57d030f8a28c75756c5ab7afa748b
정현정 <wgwjh05169@gmail.com>
2025-03-19 13:50:20 +0900

    Refactor: 테이블 초기화 JS 정리

--- util/initialize.js
+++ util/initialize.js
@@ -5,22 +5,41 @@ const { people, levels } = require('./const');
 const today = new Date();
 const weekdays = ['일', '월', '화', '수', '목', '금', '토'];
 
-// 테이블을 이번달로 초기화
-fs.readFile('../profile/README.md', 'utf8', (err, data) => {
-    if (err) {
-        console.error('파일 읽기 오류:', err);
+/**
+ * 테이블 초기화
+ * 리드미 읽기 -> 테이블 초기화 -> 이번 달 행 삽입 -> 리드미 저장
+ */
+
+async function initializeTable() {
+    /*
+    * 리드미 읽기
+    * README 파일을 읽어 document를 구성합니다.
+    */
+    let readmeData;
+    try {
+        readmeData = await fs.promises.readFile('../profile/README.md', 'utf8');
+        console.log('[INFO] README 파일 열기 성공')
+    } catch (err) {
+        console.error('[ERROR] README 파일 열기 실패: ', err);
         return;
     }
-    const dom = new JSDOM(data, { contentType: "text/html" });
+
+    const dom = new JSDOM(readmeData, { contentType: "text/html" });
     const document = dom.window.document;
 
-    const start = new Date(today.getFullYear(), today.getMonth(), 1);
-    const end = new Date(today.getFullYear(), today.getMonth() + 1, 0);
 
-    // 테이블 바디 초기화
+    /**
+     * 테이블 바디 초기화
+     */
     const tableBody = document.getElementById('problem-solve-table-body');
     tableBody.innerHTML = '';
 
+    /**
+     * 테이블 행 삽입
+     * 구조: 행 태그 > 날짜 태그 > 사람 태그 > 레벨 태그
+     */
+    const start = new Date(today.getFullYear(), today.getMonth(), 1);
+    const end = new Date(today.getFullYear(), today.getMonth() + 1, 0);
     for (let date = start; date <= end; date.setDate(date.getDate() + 1)) {
         const day = date.getDate().toString().padStart(2, '0');
         const month = (date.getMonth() + 1).toString().padStart(2, '0');
@@ -43,15 +62,22 @@ fs.readFile('../profile/README.md', 'utf8', (err, data) => {
         tableBody.innerHTML += rowText;
     }
 
+
+    /**
+     * 리드미 저장
+     */
     // html 태그 제거
     let text = dom.serialize();
     text = text.replace(/<html><head><\/head><body>/g, '\n').replace(/<\/body><\/html>/g, '');
 
-    fs.writeFile('../profile/README.md', text, 'utf8', (err) => {
-        if (err) {
-            console.error('파일 저장 오류:', err);
-        } else {
-            console.log('파일이 성공적으로 저장되었습니다.');
-        }
-    });
-});
+    // 리드미 업데이트
+    try {
+        await fs.promises.writeFile('../profile/README.md', text, 'utf8');
+        succed = true;
+        console.log('[INFO] README 파일 저장 성공');
+    } catch (err) {
+        console.error('[ERROR] README 파일 저장 실패:', err);
+    }
+}
+
+initializeTable();
