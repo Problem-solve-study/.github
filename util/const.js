2b71ba7bc1036ba9affc9ecd8bbec4a8a771b889
HyeonJeong <117988360+wgwjh05169@users.noreply.github.com>
2025-06-02 19:30:58 +0900

    Feat: 마지막 해시 업데이트

--- util/const.js
+++ util/const.js
@@ -21,7 +21,7 @@ const levels = [
 
 const blankImgTag ='<img src="blank.svg" height="20px" />';
 
-const lastUpdate = 'f16dd277d1d0fe70b0fbe3268da67844385d09c3';
+const lastUpdate = 'f5ddfb99c48e0f77be344a791cae9690653747c7';
 
 module.exports = { people, levels, lastUpdate, blankImgTag };
 
